388f95b7f0d517efbff3d4a0b4272478b7afd048
Евгений Политыко <Zhenya-623@internet.ru>
2022-09-30 10:03:05 +0300

    homeWork ready

---
+++ task01/Program.cs
@@ -0,0 +1,38 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и 
+возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+
+//метод для получения первого и второго числа
+int ReadData(string line)
+{
+    Console.WriteLine(line);
+    int number = int.Parse(Console.ReadLine() ?? "");
+    return number;
+}
+
+//метод для вычиления результата возведения числа А в степень числа В
+double FindNumber(double firstNumber, double secondNumber)
+{
+    
+    double result = Math.Pow(firstNumber, secondNumber);
+    return result;
+}
+
+//метод вывода результата на консоль
+void PrintData(string prefix, double value)
+{
+    Console.WriteLine(prefix + value);
+}
+
+//программа для получения пераого и второго числа путем ввода с консоли
+double firstNumber = ReadData("Введите первое число: ");
+double secondNumber = ReadData("Введите второе число: ");
+
+//получение результата с использованием метода FindNunber
+double outline = FindNumber(firstNumber, secondNumber);
+
+//вывод результата на консоль
+PrintData("Первое число возведённое в степень второго числа равно: ", outline);
---
+++ task03/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+ввод элеменетов через консоль (через запятую. использовать метод string.Split())*/
+
+
+//метод преобразования массива с испольованием функции split
+string[] PrintArray(string arrayStr)
+{
+
+    string [] array = arrayStr.Split(", ");
+
+    return array;
+}
+
+//команда для ввода массива
+Console.WriteLine("Введите массив:");
+//в переменную arrayStr передаем введенные числа
+string arrayStr = Console.ReadLine()??"";
+// вводим переменную в которую закладываем число с длинной массива
+int arrayLength = arrayStr.Length;
+// преобразовываем массив с использованием метода PrintArray
+string [] array = PrintArray(arrayStr);
+//цикл вывода массива 
+for (var i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]}, ");
+}
+
